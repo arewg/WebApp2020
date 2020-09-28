6d90e291235635bf51ee269db15c308c64542838
Are <aregulliksen+github@gmail.com>
2020-09-28 23:49:46 +0200

    Everything seems to be up and running with the initial set up

---
+++ Oppgave5/src/App.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import Title from './components/Title';
+
+const App = () => (
+
+<Title title="HIOF" />
+
+);
+
+export default App;
